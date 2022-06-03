c2ffd36af6f4a72af8c65c06af47c02b516e65f9
Nada Askar <nadaaskar@aucegypt.edu>
2022-06-04 01:04:57 +0200

    feat: created not-found page to which unknown routes redirect

--- app/app.module.js
+++ app/app.module.js
@@ -3,7 +3,8 @@ import 'ngVue';
 import 'ngVue/build/plugins.js';
 import PerformancePageComponent from './pages/performance-page.vue';
 import PerformanceChartComponent from './components/vue-components/performance-chart.vue';
-
+import NotFoundPageComponent from './pages/not-found-page.vue';
+import NotFoundComponent from './components/vue-components/not-found.vue';
 angular.module('appModule', [
   'ui.router',
   'ngVue',
@@ -16,4 +17,12 @@ angular.module('appModule').directive('vPerformancePage', (createVueComponent) =
 
 angular.module('appModule').directive('vPerformanceChart', (createVueComponent) => {
   return createVueComponent(Vue.component('performanceChartComponent', PerformanceChartComponent));
+});
+
+angular.module('appModule').directive('vNotFoundPage', (createVueComponent) => {
+  return createVueComponent(Vue.component('notFoundPageComponent', NotFoundPageComponent));
+});
+
+angular.module('appModule').directive('vNotFound', (createVueComponent) => {
+  return createVueComponent(Vue.component('notFoundComponent', NotFoundComponent));
 });
---
+++ app/components/vue-components/not-found.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 class="c-not-found__number">404</h1>
+        <h1 class="c-not-found__txt">Page Not Found</h1>
+        <h2 class="c-not-found__txt">The Page you are looking for does not exist.</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NotFoundComponent",
+};
+</script>
