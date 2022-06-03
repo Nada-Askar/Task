5ef21ba1a176a0ac8340ff63d9c6a7b6b7e31aaa
Nada Askar <nadaaskar@aucegypt.edu>
2022-06-03 19:32:53 +0200

    feat: created Sales Team Table Filter

--- app/components/employees-list/employees-list.component.js
+++ app/components/employees-list/employees-list.component.js
@@ -6,7 +6,21 @@ angular
     controllerAs: 'EmployeesListComponentVm',
     bindings: {
       employeesList: '<',
+      searchValue: '<',
     },
   });
-
-function EmployeesListComponent() {}
+angular
+  .module('appModule').filter('highlightSearchWord', function ($sce) {
+    return function (text, searchWord) {
+      if (!searchWord) {
+        return $sce.trustAsHtml(text);
+      }
+      return $sce.trustAsHtml(
+        text.replace(
+          new RegExp(searchWord, 'gi'),
+          '<span class="highlight">' + searchWord + '</span>'
+        )
+      );
+    };
+  });
+function EmployeesListComponent() { }
